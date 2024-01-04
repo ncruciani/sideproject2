dd08540bfc132bd71d5827cd9305ab064590db92
Nikolas git <ncruciani23@gmail.com>
2024-01-04 12:42:23 -0500

    java solo project using springtool suite

--- developerproject/src/main/java/com/nikcruciani/developerproject/controllers/HomeController.java
+++ developerproject/src/main/java/com/nikcruciani/developerproject/controllers/HomeController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
